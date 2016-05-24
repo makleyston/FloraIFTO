82374109add4608a3e27f876df6479e28a42e54a
Danne Makleyston <dannemakleyston@yahoo.com.br>
2016-05-24 17:10:43 -0300

    Update README.md
    Upando o projeto

---
+++ app/src/androidTest/java/br/ifto/projeto/ifverde/ifverde/ApplicationTest.java
@@ -0,0 +1,13 @@
+package br.ifto.projeto.ifverde.ifverde;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
